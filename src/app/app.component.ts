f01943c57416c063d3e4cf8a318ec68540b2cc65
Rob Das <noreply@dev2>
2024-07-30 16:44:10 -0600

    stubbed out the top level components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-18-shopping v0.0.6';
+  title = 'ng-18-shopping v0.0.7';
 }
