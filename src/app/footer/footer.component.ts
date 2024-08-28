841a035462b51103f12c167dac20347b6ffba804
Rob Das <noreply@dev2>
2024-08-27 19:59:55 -0600

    replaced custom link buttons and their custom css with bootstrap nav-pills

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   appName = 'DTEK Online Shopping';
-  appVersion = 'v0.2.6';
+  appVersion = 'v0.3.0';
   appDescription = 'The best place to shop online';
   currentYear = new Date().getFullYear();
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbNavModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'] // Corrected from 'styleUrl' to 'styleUrls'
 })
 export class HeaderComponent {
   title = 'DTEK Online Shopping';
+  active = 1;
 }
