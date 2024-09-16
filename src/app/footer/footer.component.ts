3345b5a5cc33ce2602c1cdbda5d5d846db646eca
Rob Das <noreply@dev2>
2024-09-16 16:29:43 -0600

    some refactoring for consistent use of boostrap container class

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -21,7 +21,7 @@ export class FooterComponent {
   appName = 'DTEK Online Shopping';
 
   // The current version of the application
-  appVersion = 'v0.4.10';
+  appVersion = 'v0.4.11';
 
   // A brief description of the application
   appDescription = 'an Angular development project. This simulated eCommerce site is ' +
