c8f2ce440b91354f605f39558df59be2365e5da5
Rob Das <noreply@dev2>
2024-09-16 17:37:55 -0600

    merged the use of three bootstrap containers into one

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -21,7 +21,7 @@ export class FooterComponent {
   appName = 'DTEK Online Shopping';
 
   // The current version of the application
-  appVersion = 'v0.4.12';
+  appVersion = 'v0.4.13';
 
   // A brief description of the application
   appDescription = 'an Angular development project. This simulated eCommerce site is ' +
