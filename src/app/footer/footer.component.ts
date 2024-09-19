7674e99c8ef7afbad72dbba27ddc55f2d344b93d
Rob Das <noreply@dev2>
2024-09-19 01:29:44 -0600

    body and app section background color tweaks

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -21,7 +21,7 @@ export class FooterComponent {
   appName = 'DTEK Online Shopping';
 
   // The current version of the application
-  appVersion = 'v0.4.21';
+  appVersion = 'v0.4.22';
 
   // A brief description of the application
   appDescription = 'an Angular development project. This simulated eCommerce site is ' +
