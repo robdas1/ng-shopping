a802a85c1def77835273e6b1fabcae645628b3e2
Rob Das <noreply@dev2>
2024-09-19 19:18:00 -0600

    fine tuning of layout

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -21,7 +21,7 @@ export class FooterComponent {
   appName = 'DTEK Online Shopping';
 
   // The current version of the application
-  appVersion = 'v0.5.1';
+  appVersion = 'v0.5.2';
 
   // A brief description of the application
   appDescription = 'an Angular development project. This simulated eCommerce site is ' +
